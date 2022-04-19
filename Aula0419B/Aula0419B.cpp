1533fc9f1c027ac47c0a53aa23ac94a90726f858
Caio Barbosa <ciruscaio@hotmail.com>
2022-04-19 19:56:51 -0300

    Segundo exerrcício

---
+++ Aula0419B/Aula0419B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 3;
+
+    if (x % 2 == 0) {
+        cout << "O numero " << x << " e PAR";
+    }
+    else{
+        cout << "O numero " << x << " e IMPAR";
+    }
+
+
+}
